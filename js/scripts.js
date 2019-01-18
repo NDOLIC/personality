c40538f58e4a710dc7791465afb3c85302d65375
NDOLIC <tblaguese1@gmail.com>
2019-01-18 10:45:20 +0200

    adding chat-box

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+ function openForm() {
+     document.getElementById("myForm").style.display = "block";
+    }
+   
+    function closeForm() {
+    document.getElementById("myForm").style.display = "block";
+    }
+var message = document.getElementById("message");
+var texBox = document.getElementById("textbox");
+var button = document.getElementsByClassName("btn");
+var button1 = document.getElementsByClassName("btn cancel");
+
+button.addEventListener("click", function () {
+    var newMessage = document.createElement("li");
+    newMessage.innerHTML = textbox.value;
+    messages.appendChild(newMessage);
+    textbox.value = "";
+});
+
+
